9299172c57ef27af352831cee0fd5334eee7e640
Carson Wardell <46233133+cwardell97@users.noreply.github.com>
2021-01-12 20:32:25 -0700

    y_t to tensor for ms get reward method

--- src/models/_rl_helpers.py
+++ src/models/_rl_helpers.py
@@ -71,7 +71,7 @@ def get_reward_ms(a_t, y_t, penalty, allow_dk=True):
     """
     dk_id = y_t.shape[0]
     # if y_t is all zeros (delay period), then action target DNE
-    if torch.all(y_t == 0):
+    if torch.all(torch.from_numpy(y_t) == 0):
         # -1 is not in the range of a_t, so r_t = penalty unless a_t == dk
         a_t_targ = torch.tensor(-1)
     else:
