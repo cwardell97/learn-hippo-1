dc999e2f120ceee3f5213770f9b23767dc8e5113
Carson Wardell <46233133+cwardell97@users.noreply.github.com>
2021-02-09 18:39:16 -0500

    size() --> .shape for type compatibility

--- src/models/_rl_helpers.py
+++ src/models/_rl_helpers.py
@@ -28,7 +28,7 @@ def get_reward(a_t, y_t, penalty, allow_dk=True):
         immediate reward at time t
 
     """
-    dk_id = y_t.size()[0]
+    dk_id = y_t.shape[0]
     # if y_t is all zeros (delay period), then action target DNE
     if torch.all(torch.from_numpy(y_t) == 0):
         # -1 is not in the range of a_t, so r_t = penalty unless a_t == dk
