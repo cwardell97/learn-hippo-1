33f25a6bd7bd80bf2d4b6a7e99a753d49da0d2e5
Carson Wardell <46233133+cwardell97@users.noreply.github.com>
2021-03-17 00:19:13 -0400

    new try its

--- src/eval-retrain-cw.py
+++ src/eval-retrain-cw.py
@@ -11,8 +11,8 @@ from utils.params import P
 from utils.io import build_log_path, load_ckpt, pickle_save_dict, \
     get_test_data_dir, get_test_data_fname, load_env_metadata, pickle_load_dict
 
-plt.switch_backend('agg')
-sns.set(style='white', palette='colorblind', context='talk')
+#plt.switch_backend('agg')
+#sns.set(style='white', palette='colorblind', context='talk')
 
 ''' remove parser capability for now
 parser = argparse.ArgumentParser()
@@ -80,7 +80,7 @@ log_root = args.log_root
 
 log_root = '/Users/carsonwardell/Desktop/Thesis/log/'
 
-exp_name = 'vary-test-penalty'
+exp_name = 'vary-test-penalty(trained)'
 def_prob = None
 n_def_tps = 0
 
@@ -123,6 +123,7 @@ penaltys_train = [4]
 penaltys_test = np.array([2])
 
 all_conds = ['DM']
+scramble = False
 
 for slience_recall_time in slience_recall_times:
     for subj_id, penalty_train, fix_cond in product(subj_ids, penaltys_train, all_conds):
@@ -145,8 +146,7 @@ for slience_recall_time in slience_recall_times:
             )
             # create logging dirs
             log_path, log_subpath = build_log_path(
-                subj_id, p, log_root=log_root, mkdir=False, verbose=False
-            )
+                subj_id, p, log_root=log_root, mkdir=False, verbose=False)
 
             # create fpath
             test_params = [fix_penalty, pad_len_test, slience_recall_time]
@@ -162,6 +162,12 @@ for slience_recall_time in slience_recall_times:
                 if not os.path.exists(test_data_dir):
                     os.makedirs(test_data_dir)
             fpath = os.path.join(test_data_dir, test_data_fname)
+            print(fpath)
+            print(test_data_fname)
+            print(test_data_dir)
+            print(log_subpath)
+            print(fpath.split("sup_epoch-600",1)[1])
+
 
             # init env
             env_data = load_env_metadata(log_subpath)
@@ -237,9 +243,82 @@ for slience_recall_time in slience_recall_times:
 
 
 
-'''
-playing with data:
-print(training_data)
+
+#playing with data:
+print(fpath)
+import pickle
+data = pickle.load(open('/Users/carsonwardell/Desktop/Thesis/n256.pkl', "rb"))
+
+sims_data = data.pop('sims_data')
+
+sims_data_1 = sims_data[0]
+sims_data_2 = sims_data[1]
+sims_data_1[0]
+np.shape(sims_data_2)
+plt.plot(sims_data_2)
+
+
+
+
+
+
+
+
+
+
+
+
+A= 3
+T= 16
+t= 14
+a_t = 1
+
+from utils.utils import to_sqnp
+
+time_oh_vec = np.identity(T)[t%T]
+time_q_vec = np.identity(T)[t%T]
+# special case if dk
+if a_t == A:
+    obs_val_oh_vec =  np.zeros(A)
+else:
+    obs_val_oh_vec = np.identity(A)[a_t]
+
+retrn1 = np.concatenate([time_oh_vec,obs_val_oh_vec, time_oh_vec])
+retrn1.shape
+np.shape(to_sqnp(retrn1))
+
+
+boop = np.reshape(retrn1, (np.shape(retrn1)[0],1))
+boop = np.reshape(obs_val_oh_vec, (np.shape(obs_val_oh_vec)[0],1))
+boop = np.reshape(time_oh_vec, (np.shape(time_oh_vec)[0],1))
+plt.imshow(boop.T)
+boop.shape
+print(np.shape(time_oh_vec))
+print(np.shape(obs_val_oh_vec))
+
+np.shape(np.concatenate([time_oh_vec,obs_val_oh_vec]))
+
+t=2
+boop = []
+for i in range(t,5):
+    boop.append(0)
+
+boop
+
+np.shape(retrn1)
+trans_retrn = np.atleast_2d(retrn).T.shape
+np.shape(trans_retrn)
+log_rets = np.empty(trans_retrn.shape)
+np.atleast_2d(retrn).T.shape
+log_rets.shape
+log_rets = np.vstack([log_rets, retrn])
+log_rets = np.vstack([log_rets, retrn1])
+log_rets.shape[1]
+plt.imshow(log_rets)
+
+training_data = pickle_load_dict(fpath).pop('XY')
+X_train = np.array(training_data[0])
+Y_train = np.array(training_data[1])
 
 X = np.array(training_data[0])
 Y = np.array(training_data[1])
@@ -247,20 +326,146 @@ Y = np.array(training_data[1])
 X.shape
 Y.shape
 
-
 import random as rd
 
 i = rd.randint(0,(n_examples_test-1))
-print(i)
-X_i = X[i,:,:]
-Y_i = Y[i,:,:]
+print("i:",i)
+X_i = X_train[i,:,:]
+Y_i = Y_train[i,:,:]
 
-Y_i.shape[1]
-X_i.shape[0]
+Y_i.shape#[1]
+X_i.shape#[0]
 import matplotlib.pyplot as plt
 plt.imshow(X_i)
 plt.imshow(Y_i)
 
+Y_i[13].shape[0]
+
+torch.from_numpy(Y_i)
+torch.argmax(torch.from_numpy(Y_i))
+
 j = rd.randint(0,(X_i.shape[0]-1))
 X_i_t0 = X_i[j,:]
-'''
+
+
+probs, rewards, values, ents = [], [], [], []
+a = torch.empty(1)
+a.add_(1)
+print(a)
+
+rewards.add(a)
+
+print(rewards)
+
+deep = np.random.choice(15,2, replace=False)
+
+
+k = 2
+n_examples = 256
+
+data_sample_ind=np.random.choice(n_examples,k,replace = False)
+X_dict = {}
+Y_dict = {}
+i=0
+for k in data_sample_ind:
+    X_dict["X_{0}".format(i)] = "Hello{0}".format(k)
+    Y_dict["Y_{0}".format(i)] = "Hello{0}".format(k)
+    i+=1
+    print(i)
+X_dict
+
+X_dict["X_{0}".format(0)]
+
+k=2
+for i in range(k):
+    print(i)
+
+seed_num = 2
+range(1,seed_num)
+j = np.random.choice(seed_num)
+print(j)
+
+for t in range(16):
+    if t<(seed_num):
+        print(t)
+
+for sn in range(seed_num):
+    print(sn)
+
+
+
+
+def io_convert(a_t, t, T, A):
+    '''converts model output to one-hot vector input format
+    '''
+    time_oh_vec = np.identity(T)[t%T]
+    time_q_vec = np.identity(T)[t%T]
+    # special case if dk
+    if a_t == A:
+        obs_val_oh_vec =  np.zeros(A)
+    else:
+        obs_val_oh_vec = np.identity(A)[a_t]
+    return np.concatenate([time_oh_vec,obs_val_oh_vec, time_oh_vec])
+
+A = np.array([0., 0., 0., 1.]).shape[0]
+print(A)
+T = 16
+t = 1
+a_t = 3
+
+io_convert(a_t, t, T, A)
+# play with results
+dpath = '/Users/carsonwardell/Desktop/Thesis/log/subj-1(working-2:22)'
+
+mod_data = pickle_load_dict(dpath)
+
+seed_num = 2
+X_dict = [0,1]
+print(len(X_dict))
+j = np.random.choice(len(X_dict))
+print("j", j)
+
+T_part = 16
+for t in range(T_part):
+    print(t
+
+
+deep = np.zeros(40)
+
+deep_short = deep[:20,]
+
+np.shape(deep_short)
+
+# compare two X_array_list
+
+outp = np.array([1,1,0,0])
+fakemem = np.array([1,0,0,0])
+fakemem2 = np.array([1,0,0,0])
+
+if not np.all(outp == fakemem) and not np.all(outp == fakemem2):
+    print("other")
+
+mem_1_comp
+
+print(np.sum([[3],[1],[0]]))
+
+
+
+rewards = [torch.tensor(-5.)]
+R = 0
+gamma=0
+returns = []
+for r in rewards[::-1]:
+    R = r + gamma * R
+    returns.insert(0, R)
+returns = torch.tensor(returns)
+# normalize w.r.t to the statistics of this trajectory
+
+print(returns)
+
+deep = np.zeros((5,16))
+print(np.shape(np.amax(deep, axis = 1)))
+
+mem_num = 2
+for mn in range(mem_num):
+    print(mn)
