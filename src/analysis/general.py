cff668eebcccb7b082a99959fa4e83962af0aee7
Carson Wardell <46233133+cwardell97@users.noreply.github.com>
2021-03-17 00:18:56 -0400

    new stat function

--- src/analysis/general.py
+++ src/analysis/general.py
@@ -84,3 +84,31 @@ def compute_stats_max(matrix, axis=0, n_se=2):
     mu_ = np.amax(matrix, axis=axis)
     er_ = sem(matrix, axis=axis) * n_se
     return mu_, er_
+
+
+def n_epoch_inpt_calc(inpts, sems, n, axis=0):
+    """compute mean inpt gate activation curve of first and last n epochs
+
+    Parameters
+    ----------
+    matrix : type
+        Description of parameter `matrix`.
+    axis : type
+        Description of parameter `axis`.
+    n : type
+        number of first and last epochs to be grabbed.
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
+    mu_first = np.mean(inpts[:n,:], axis=axis)
+    er_first = np.mean(sems[:n,:], axis=axis)
+
+    mu_last = np.mean(inpts[-n:,:], axis=axis)
+    er_last = np.mean(sems[-n:,:], axis=axis)
+
+
+    return mu_first, er_first, mu_last, er_last
